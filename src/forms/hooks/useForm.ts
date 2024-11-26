926fd7223a32eb66698e46e4a79ad40d7e1e4e75
luciano-dlr <delarubialuciano@gmail.com>
2024-11-26 19:27:56 -0300

    feature(validates): create validations on inputs and reset () from values

--- src/forms/hooks/useForm.ts
+++ src/forms/hooks/useForm.ts
@@ -2,9 +2,9 @@ import { ChangeEvent, useState } from "react";
 
 
 
-export const useForm = <T>(initualState: T) => {
+export const useForm = <T>(initialState: T) => {
 
-    const [formData, setFormData] = useState(initualState)
+    const [formData, setFormData] = useState(initialState)
 
     const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
         // console.log(event.target.value);
@@ -15,10 +15,21 @@ export const useForm = <T>(initualState: T) => {
         }))
     }
 
+    const handleReset = () => {
+        setFormData({ ...initialState })
+    }
+
+    const handleValidateEmail = (email: string) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
 
     return {
         ...formData,
         handleOnChange,
-        formData
+        formData,
+        handleReset,
+        handleValidateEmail
     }
 }
--- src/forms/pages/RegisterPage.tsx
+++ src/forms/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import { FormEvent } from "react"
 
 
 import "../styles/styles.css"
@@ -6,14 +6,13 @@ import { useForm } from "../hooks/useForm"
 
 export const RegisterPage = () => {
 
-    const { formData, handleOnChange } = useForm({
-        name: '',
-        email: '',
+    const { handleValidateEmail, formData, handleOnChange, name, email, password1, password2, handleReset } = useForm({
+        name: 'Luciano',
+        email: 'rubia@gmail.com',
         password1: '',
         password2: ''
     })
 
-    const { name, email, password1, password2 } = formData
 
 
     const onSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -32,15 +31,22 @@ export const RegisterPage = () => {
                     name="name"
                     value={name}
                     onChange={(event) => handleOnChange(event)}
+                    className={`${name.trim().length <= 0 && 'has-error'} `}
                 />
+
+                {name.trim().length <= 0 && <span>Este Campo es Obligatorio</span>}
                 <input
                     type="email"
                     placeholder="Email"
                     name="email"
                     value={email}
                     onChange={(event) => handleOnChange(event)}
+                    className={`${!handleValidateEmail(email) && 'has-error'} `}
+
 
                 />
+                {!handleValidateEmail(email) && <span>Email no Valido</span>}
+
                 <input
                     type="password"
                     placeholder="Paswword"
@@ -49,6 +55,9 @@ export const RegisterPage = () => {
                     onChange={(event) => handleOnChange(event)}
 
                 />
+                {password1.trim().length <= 0 && <span>Este Campo es Obligatorio</span>}
+                {password1.trim().length < 6 && password1.trim().length > 0 && <span>Minimo 6 caracteres</span>}
+
                 <input
                     type="password"
                     placeholder="Confrim Password"
@@ -57,7 +66,11 @@ export const RegisterPage = () => {
                     onChange={(event) => handleOnChange(event)}
 
                 />
+                {password2.trim().length <= 0 && <span>Este Campo es Obligatorio</span>}
+                {password2 !== password1 && <span>Las contraseñas deben ser iguales</span>}
+
                 <button type="submit">Submit</button>
+                <button type="button" onClick={handleReset}>Reset</button>
             </form>
         </div>
     )
